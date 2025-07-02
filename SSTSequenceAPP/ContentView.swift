1fabb25ff943522ef903a9c98dbb3d11058c16c1
Shiene Park <logintobuysome@gmail.com>
2025-07-03 08:17:46 +0900

    Initial Commit

---
+++ SSTSequenceAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SSTSequenceAPP
+//
+//  Created by superUser on 7/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SSTSequenceAPP/SSTSequenceAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSTSequenceAPPApp.swift
+//  SSTSequenceAPP
+//
+//  Created by superUser on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SSTSequenceAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
